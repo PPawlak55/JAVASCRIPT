95fc3cbfb31daf3810967390604a8dd5ff2ffb22
Patryk Pawlak <patrykxpawlak@gmail.com>
2022-11-24 12:51:08 +0100

    GRA-GWENT

---
+++ memory.js
@@ -0,0 +1,5 @@
+
+var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier", "iorweth.png", "triss.png", "geralt.png",
+"yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
+
+alert(cards[4]);
